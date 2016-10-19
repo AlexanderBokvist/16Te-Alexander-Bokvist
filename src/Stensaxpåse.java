a8aaae1c0b3a12ddcebf74c11f023264059ef419
AlexanderBokvist <alexander.bokvist@elev.it-gymnasiet.se>
2016-10-19 11:21:56 +0200

    test

---
+++ src/Stensaxpåse.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class Stensaxpåse {
+
+    public static void main(String[] args) {
+
+int dator;
+int spelare;
+
+String.[] alternativ = {"Sten", "Sax", "Påse"}
+
+System.out.println("sten sax påse!");
+System.out.println("Sten=0 Sax=1 Påse= 2");
+Scanner spelarval = new Scanner(System.in);
+spelare = in.nextInt();
+do {
+spelare = spelarval.nextInt();
+} while (!((spelare <3) && (spelare <==0));
+dator = (int) (Math.random()*3);
+
+
+System.out.println("x");
+
+
+
+{
+if ((spelare ==1 && dator ==2) ||
+        ((spelare ==0 && dator ==1)) ||
+        (spelare ==2 && dator == 0) ||
+        
+          System.out.println("Du vann")
+   
+        
+    }
+
+    }
+
+    private static class in {
+
+        private static int nextInt() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        public in() {
+        }
+    }
